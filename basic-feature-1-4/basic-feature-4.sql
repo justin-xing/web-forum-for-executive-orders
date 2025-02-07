25c057565edf7bdb0dff6ba48f062e94e5363e0e
pane2004 <alexlu2000clash@gmail.com>
2025-02-07 15:42:03 -0500

    complete basic feature 4

--- basic-feature-1-4/basic-feature-4.sql
+++ basic-feature-1-4/basic-feature-4.sql
@@ -1,14 +1,22 @@
-SELECT c.comment_id, c.message, (
-SELECT count(uid)
-FROM VotesFor
-WHERE is_upvote=1 AND comment_id=c.comment_id
-) - (
-SELECT count(uid)
-FROM VotesFor
-WHERE is_upvote=0 AND comment_id=c.comment_id
-) AS vote_score
-FROM Documents d
-LEFT JOIN IsCommentFor c
-	ON c.executive_order_id = d.executive_order_id
-WHERE d.executive_order_id = 12 -- test id
-ORDER BY vote_score DESC
+SELECT 
+  c.cid,
+  c.uid,
+  c.message,
+  (
+    SELECT COUNT(uid) 
+    FROM VoteFor v
+    WHERE v.cid = c.cid 
+      AND v.is_upvote = 1
+  )
+  -
+  (
+    SELECT COUNT(uid) 
+    FROM VoteFor v
+    WHERE v.cid = c.cid 
+      AND v.is_upvote = 0
+  ) AS vote_score
+FROM Document d
+JOIN Comment c 
+  ON d.executive_order_id = c.executive_order_id
+WHERE d.executive_order_id = 14195 -- test id
+ORDER BY vote_score DESC;
