e244bd0bfddc4fbba1918ac1e7c6753129aca54e
pane2004 <alexlu2000clash@gmail.com>
2025-03-14 17:28:36 -0400

    update f1 query

--- basic-feature-1-4/basic-feature-1.sql
+++ basic-feature-1-4/basic-feature-1.sql
@@ -1,6 +1,6 @@
 SELECT *
 FROM Document
 WHERE president = 'Donald Trump'
-  AND signing_date BETWEEN '2021-01-01' AND '2021-12-31'
+  AND signing_date BETWEEN '2017-01-01' AND '2019-12-31'
   AND tag = 'Immigration';
 
